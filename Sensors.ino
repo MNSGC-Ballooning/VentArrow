e7a75f79741091cbf10e0509530f06818be6c5ab
Ryan Bowers <rbowers314@gmail.com>
2016-07-27 08:28:32 -0600

    added No Fix message in datalog; added GPS and xBee functions to loop()

--- Sensors.ino
+++ Sensors.ino
@@ -2,20 +2,26 @@ unsigned long timer = millis();
 byte logRate = 1;   //interval in seconds between cycles of datalogging
 
 void updateGPS() {
-  while (gpsSerial.available() > 0){
+  while (gpsSerial.available() > 0) {
     GPS.read();
-    if (GPS.newNMEAreceived()) break;
+    if (GPS.newNMEAreceived()) {
+      GPS.parse(GPS.lastNMEA());
+      break;
+    }
   }
-  GPS.parse(GPS.lastNMEA());
-  if (millis() - timer < logRate * 1000) {
+  if (millis() - timer > logRate * 1000) {
     timer = millis();
-    openDatalog();
-    datalog.print(flightTimeStr() + "," + String(GPS.latitudeDegrees) + "," + String(GPS.longitudeDegrees) + ",");
-    datalog.print(String(GPS.altitude * 3.28048) + ",");    //convert meters to feet for datalogging
-    datalog.print(String(GPS.month) + "/" + String(GPS.day) + "/" + String(GPS.year) + ",");
-    datalog.print(String(GPS.hour) + ":" + String(GPS.minute) + ":" + String(GPS.seconds) + ",");
-    closeDatalog();
+    if (GPS.fix) {
+      openDatalog();
+      datalog.print(flightTimeStr() + "," + String(GPS.latitudeDegrees) + "," + String(GPS.longitudeDegrees) + ",");
+      datalog.print(String(GPS.altitude * 3.28048) + ",");    //convert meters to feet for datalogging
+      datalog.print(String(GPS.month) + "/" + String(GPS.day) + "/" + String(GPS.year) + ",");
+      datalog.println(String(GPS.hour) + ":" + String(GPS.minute) + ":" + String(GPS.seconds) + ",");
+      closeDatalog();
+    }
+    else
+      datalog.println(flightTimeStr() + ",No fix");
   }
-  
+
 }
 
